a6518af337168caf899b7cf3a3fbd14327ece822
keejuanfoo <fookeejuan@gmail.com>
2026-08-19 22:21:36 +0800

    Update code to echo commands entered by user

--- src/main/java/Crow.java
+++ src/main/java/Crow.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Starts the CROW chatbot application.
  */
@@ -14,7 +16,20 @@ public class Crow {
         System.out.println("Hello! I'm Crow.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
