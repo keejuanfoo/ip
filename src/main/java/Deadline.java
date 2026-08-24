ea741b6875a678a0813a9713cce2feab45b974dd
keejuanfoo <fookeejuan@gmail.com>
2026-08-24 17:44:59 +0800

    Use java.time to handle date and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,31 +1,38 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 /**
  * Represents a task that must be completed by a specific date or time.
  */
 public class Deadline extends Task {
-    protected String by;
+    private static final DateTimeFormatter DISPLAY_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy h:mma", Locale.ENGLISH);
+
+    protected LocalDateTime by;
 
     /**
      * Creates a deadline task.
      *
      * @param description Description of the task.
-     * @param by Date or time by which the task must be completed.
+     * @param by Date and time by which the task must be completed.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     /**
-     * Returns the deadline as entered by the user.
+     * Returns the deadline date and time.
      *
-     * @return Deadline text.
+     * @return Deadline date and time.
      */
-    public String getBy() {
+    public LocalDateTime getBy() {
         return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DISPLAY_FORMAT) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,43 +1,51 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 /**
  * Represents a task that occurs between a start and end date or time.
  */
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    private static final DateTimeFormatter DISPLAY_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy h:mma", Locale.ENGLISH);
+
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     /**
      * Creates an event task.
      *
      * @param description Description of the event.
-     * @param from Start date or time.
-     * @param to End date or time.
+     * @param from Start date and time.
+     * @param to End date and time.
      */
-    public Event(String description, String from, String to) {
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
     /**
-     * Returns the event's start as entered by the user.
+     * Returns the event's start date and time.
      *
-     * @return Start text.
+     * @return Start date and time.
      */
-    public String getFrom() {
+    public LocalDateTime getFrom() {
         return from;
     }
 
     /**
-     * Returns the event's end as entered by the user.
+     * Returns the event's end date and time.
      *
-     * @return End text.
+     * @return End date and time.
      */
-    public String getTo() {
+    public LocalDateTime getTo() {
         return to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(DISPLAY_FORMAT)
+                + " to: " + to.format(DISPLAY_FORMAT) + ")";
     }
 }
